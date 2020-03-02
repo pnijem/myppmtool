7ff71a4b45eeb6be54c0e51a103a1adaeff08d08
peternijem <peter@presenso.com>
2020-03-02 22:15:30 +0200

    Delete directory

--- ppm-service/src/main/java/com/pnijem/ppmtool/repositories/BacklogRepository.java
+++
@@ -1,8 +0,0 @@
-package com.pnijem.ppmtool.repositories;
-
-import com.pnijem.ppmtool.domain.Backlog;
-import org.springframework.data.repository.CrudRepository;
-
-public interface BacklogRepository extends CrudRepository<Backlog, Long> {
-    
-}
--- ppm-service/src/main/java/com/pnijem/ppmtool/repositories/ProjectTaskRepository.java
+++
@@ -1,9 +0,0 @@
-package com.pnijem.ppmtool.repositories;
-
-import com.pnijem.ppmtool.domain.ProjectTask;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProjectTaskRepository extends CrudRepository<ProjectTask, Long> {
-
-
-}
